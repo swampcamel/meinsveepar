a03700c1827e927913d81fcb41e6fe4bd0ffe3c6
Dan Mace and Josh Gearheart <dan@>
2018-11-29 11:15:28 -0800

    get recursive reducer going

--- src/Square.jsx
+++ src/Square.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Square.scss';
 
 function Square(props) {
-  console.log(props)
   return (
     <div className="square">{props.id}</div>
   )
--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -1,11 +1,23 @@
 export default (state = {gameBoard: Array(25).fill({bomb: false})}, action) => {
 
+  const loopRecursor = (squareArray) => {
+    let randomSquareIndex = Math.floor(((Math.random()*25)));
+    let squareToCheck = squareArray[randomSquareIndex];
+    if (squareToCheck == {bomb: true}){
+      console.log("recursing")
+      loopRecursor(squareArray);
+    } else {
+      return randomSquareIndex;
+    }
+  }
+
   switch (action.type) {
     case 'NEW_GAME':
-      let newState = Object.assign({}, state);
-      newState.gameBoard.forEach(square => square.bomb = false);
+      let newState = Object.assign({}, {gameBoard: Array(25).fill({bomb: false})});
+      console.log(newState.gameBoard)
       for (let i = 0; i < 6; i++) {
-        newState.gameBoard[Math.floor((Math.random()*25)+1)] = {bomb: true};
+        let squareIndex = loopRecursor(newState.gameBoard);
+        newState.gameBoard[squareIndex] = {bomb: true};
       }
       return newState;
   default:
